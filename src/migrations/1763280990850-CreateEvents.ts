8bce0d75dcf698c08ed1a8fe73f1d6afcadded78
Or Mashiach <ormashi12@gmail.com>
2025-11-19 15:21:36 +0200

    Implementing an option to upload images to the server

--- src/migrations/1763280990850-CreateEvents.ts
+++ src/migrations/1763280990850-CreateEvents.ts
@@ -4,7 +4,11 @@ export class CreateEventsTable1733289990000 implements MigrationInterface {
     name = 'CreateEventsTable1733289990000'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "events" ("Index" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "Date" varchar NOT NULL, "location" varchar NOT NULL, "typeActivity" varchar NOT NULL, "categoryoption" varchar NOT NULL, "eventSeverity" varchar NOT NULL, "typeUnitActivity" varchar NOT NULL, "weather" varchar NOT NULL, "eventDescription" varchar NOT NULL, "subSubUnitInput" varchar NOT NULL, "results" varchar NOT NULL, "injuryLevel" varchar NOT NULL)`);
+        await queryRunner.query(`CREATE TABLE "events" ("Index" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+            "Date" varchar NOT NULL, "location" varchar NOT NULL, "typeActivity" varchar NOT NULL, 
+            "categoryoption" varchar NOT NULL, "eventSeverity" varchar NOT NULL, "typeUnitActivity" varchar NOT NULL, 
+            "weather" varchar NOT NULL, "eventDescription" varchar NOT NULL, "subSubUnitInput" varchar NOT NULL, 
+            "results" varchar NOT NULL, "injuryLevel" varchar NOT NULL)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
--- src/routes/events.routes.ts
+++ src/routes/events.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
 import { EventsController } from "../controllers/events.controller";
+import multer from "multer";
+
+const upload = multer({
+  dest: "uploads/",
+});
 
 const router = Router();
 
@@ -10,5 +15,18 @@ router.post("/", EventsController.createEvent);
 router.put("/:id", EventsController.updateEvent);
 
 router.delete("/:id", EventsController.deleteEvent);
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "no image uploaded" });
+  }
+
+  return res.json({
+    message: "image uploaded successfully",
+    fileName: req.file.filename,
+    filePath: `/uploads/${req.file.filename}`,
+  });
+});
+
 
 export default router;
